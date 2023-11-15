1e74466c5c25a216609ea1bc5ad901d8504d8943
Jorge Sánchez <121513033+PapitoJorge@users.noreply.github.com>
2023-11-15 19:02:06 +0000

    finish day 4 and will fix day 5

---
+++ programa5.py
@@ -0,0 +1,10 @@
+student_heights = input().split
+
+for n in range(0, len(student_heights)):
+    student_heights[n] = int(student_heights[n])
+
+total_height = 0
+
+total_height += height
+
+print(f"total height = {total_height}")
